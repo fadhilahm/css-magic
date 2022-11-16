3f75f86154a62236e87019fd8618b17a4491bbf4
fadhilahm <fadhilahmetra@gmail.com>
2022-11-16 13:24:54 +0900

    Made the button starts rippling.

---
+++ mui-button-ripple-effect/index.js
@@ -0,0 +1,31 @@
+const createRipple = (event) => {
+  // Get our button.
+  const button = event.currentTarget;
+
+  // Instantiate the ripple.
+  const circle = document.createElement("span");
+  const diameter = Math.max(button.clientWidth, button.clientHeight);
+  const radius = diameter / 2;
+
+  // Set the size of our ripple component and add the class.
+  circle.style.width = circle.style.height = `${diameter}px`;
+  circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
+  circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
+  circle.classList.add("ripple");
+
+  // Remove existing ripple effect.
+  const ripple = button.getElementsByClassName("ripple")[0];
+  if (ripple) {
+    ripple.remove();
+  }
+
+  console.log("is this called?");
+  // Inject the effect inside the button element.
+  button.appendChild(circle);
+};
+
+// Add the effect to every button.
+const buttons = document.getElementsByTagName("button");
+for (const button of buttons) {
+  button.addEventListener("click", createRipple);
+}
