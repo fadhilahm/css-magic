c7f72b32710bdfae2a85195075677ac9cf200bf6
fadhilahm <fadhilahmetra@gmail.com>
2022-11-21 17:22:17 +0900

    Finished the instruction to select text-area automatically.

--- html-dom-dev/basic/index.js
+++ html-dom-dev/basic/index.js
@@ -8,8 +8,39 @@ console.log("Should contain the content of last child => ", last.innerHTML);
 
 // Scroll to an element
 const element2 = document.querySelector(".scroll-to-an-element__scroll-target");
-setInterval(() => {
-  console.log("scrollIntoView()");
-  //   element2.scrollIntoView();
-  element2.scrollIntoView({ behavior: "smooth" });
-}, 200);
+// setInterval(() => {
+//   console.log("scrollIntoView()");
+//   //   element2.scrollIntoView();
+//   element2.scrollIntoView({ behavior: "smooth" });
+// }, 200);
+
+// Select an element or list of elements
+console.log(
+  `Select an element by given ID => ${
+    document.getElementById("select-an-element-or-list-of-elements__id")
+      .innerHTML
+  }`
+);
+console.log(
+  `Select an element by given class name => ${document.getElementsByClassName(
+    "select-an-element-or-list-of-elements__classname"
+  )}`
+);
+console.log(
+  `Select all elements by CSS selector => ${document.querySelectorAll(
+    ".select-an-element-or-list-of-elements__selector"
+  )}`
+);
+console.log(
+  `Select the first element by CSS selector => ${document.querySelector(
+    ".select-an-element-or-list-of-elements__selector"
+  )}`
+);
+
+// Select the text of a textarea automatically
+document
+  .getElementById("select-the-text-of-a-textarea-automatically")
+  .addEventListener("focus", (e) => {
+    // Select the text
+    e.target.select();
+  });
